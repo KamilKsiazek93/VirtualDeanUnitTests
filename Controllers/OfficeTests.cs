658c0e723727a29d404c9651738a4dedbd0c6bd8
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2021-12-16 20:32:48 +0100

    Added tests for getting single brothers

--- Controllers/OfficeTests.cs
+++ Controllers/OfficeTests.cs
@@ -38,5 +38,27 @@ namespace VirtualDeanUnitTests.Controllers
                 Assert.That(result.ElementAt(1).isDiacon, Is.EqualTo(false));
             });
         }
+
+        [Test]
+        public async Task GetBrothers_WhenProvideId_ReturnsBrotherWithProvidedId()
+        {
+            var mockedBrothers = new MockedBrothers().getMockedBrothers();
+            var mockedRepositoryBrother = new Mock<IBrothers>();
+            mockedRepositoryBrother
+                .Setup(repo => repo.GetBrother(2))
+                .Returns(() => Task.FromResult(mockedBrothers.ElementAt(1)));
+            var officeController = new VirtualDean.Controllers.Offices(mockedRepositoryBrother.Object, null, null, null);
+            var result = await officeController.GetBrothers(2);
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Id, Is.EqualTo(2));
+                Assert.That(result.Name, Is.EqualTo("Bro"));
+                Assert.That(result.Surname, Is.EqualTo("OP"));
+                Assert.That(result.isSinging, Is.EqualTo(false));
+                Assert.That(result.isLector, Is.EqualTo(true));
+                Assert.That(result.isAcolit, Is.EqualTo(true));
+                Assert.That(result.isDiacon, Is.EqualTo(false));
+            });
+        }
     }
 }
